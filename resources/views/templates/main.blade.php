2bb54404def248d0b1b17e4615c9d8115633be18
chidodesigns <chidoukaigwe@gmail.com>
2022-05-21 13:52:41 +0100

    initial commit

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+
+        <title>{{ config('app.name', 'User Management Billing System')}}</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+
+        <!-- JS -->
+        <script src="{{ asset('js/app.js')}}" defer></script>
+
+    </head>
+    <body>
+        <div>
+            @if (Route::has('login'))
+                <div>
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif  
+        </div>
+        <main>
+            @yield('content')
+        </main>
+    </body>
+</html>
