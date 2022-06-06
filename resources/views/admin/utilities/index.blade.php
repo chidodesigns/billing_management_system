055648ce6784f39fbb7f9eea3937fb67e1a1f353
chidodesigns <chidoukaigwe@gmail.com>
2022-06-06 15:11:51 +0100

    updated utilities index template

--- resources/views/admin/utilities/index.blade.php
+++ resources/views/admin/utilities/index.blade.php
@@ -4,10 +4,15 @@
     <h1>Utilities</h1>
     <div class="card">
         <h4 class="my-5">Export</h4>
-        <div class="card">
-            <a href="{{route('admin.clients.export')}}" class="btn btn-lg btn-success mb-5" type="button">Export Clients</a>
-            <a href="{{route('admin.clientpayments.export')}}" class="btn btn-lg btn-success mb-5" type="button">Export Client Payment Records</a>
-            <a href="{{route('admin.servicepayments.export')}}" class="btn btn-lg btn-success mb-5" type="button">Export Service Payment Records</a>
+            <div class="div d-flex">
+                <span style="width: 25%;">Clients</span> <a href="{{route('admin.clients.export')}}" class="btn btn-sm btn-success mb-5 ms-1" type="button">Export</a>
+            </div>
+            <div class="div d-flex">
+                <span style="width: 25%;">Recurring Profiles</span> <a href="{{route('admin.clientpayments.export')}}" class="btn btn-sm btn-success mb-5 ms-1" type="button">Export</a>
+            </div>
+            <div class="d-flex">
+                <span style="width: 25%;">Service Payment Records</span> <a href="{{route('admin.servicepayments.export')}}" class="btn btn-sm btn-success ms-1 mb-5" type="button">Export</a>
+            </div>
+            
         </div>
-    </div>
 @endsection
