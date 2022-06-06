3b1cf57ba394d49121657e0b48363a0b99e08f87
chidodesigns <chidoukaigwe@gmail.com>
2022-06-06 13:50:37 +0100

    updated service payment create form

--- resources/views/admin/service-payments/partials/form.blade.php
+++ resources/views/admin/service-payments/partials/form.blade.php
@@ -7,7 +7,7 @@
 
 @if(!$edit)
 <div class="mb-3">
-    <label for="service_id" class="form-label">Service Type</label>
+    <label for="service_id" class="form-label">Service Type*</label>
     <select class="form-select" aria-label="service_id" name="service_id" id="service_id">
         @isset($services)
             @foreach ($services as $service)
@@ -20,13 +20,13 @@
 
 @if ($edit)
     <div class="mb-3">
-        <label for="service_type_name">Service Type</label>
+        <label for="service_type_name">Service Type*</label>
         <input type="text" class="form-control" id="service_type_name" name="service_type_name" value="{{$service_payment_record->service_type_name}}" disabled>
     </div>
 @endif
 
 <div class="mb-3">
-    <label for="domain" class="form-label">Domain</label>
+    <label for="domain" class="form-label">Domain*</label>
     <input type="text" class="form-control @error('domain') is-invalid @enderror" id="domain"
         aria-describedby="Domain" name="domain" value="{{ old('domain') }}@isset($service_payment_record) {{ $service_payment_record->domain }} @endisset">
     @error('domain')
@@ -36,7 +36,7 @@
     @enderror
 </div>
 <div class="mb-3">
-    <label for="registration_date" class="form-label">Registration Date</label>
+    <label for="registration_date" class="form-label">Registration Date*</label>
     <input type="date" class="form-control @error('registration_date') is-invalid @enderror" id="registration_date"
         aria-describedby="Registration Date" name="registration_date" @isset($service_payment_record) value="{{$service_payment_record->registration_date}}" @endisset>
     @error('registration_date')
@@ -46,7 +46,7 @@
     @enderror
 </div>
 <div class="mb-3">
-    <label for="renewal_date" class="form-label">Renewal Date</label>
+    <label for="renewal_date" class="form-label">Renewal Date*</label>
     <input type="date" class="form-control @error('renewal_date') is-invalid @enderror" id="renewal_date"
         aria-describedby="Registration Date" name="renewal_date"  @isset($service_payment_record) value="{{$service_payment_record->renewal_date}}" @endisset>
     @error('renewal_date')
@@ -56,7 +56,6 @@
     @enderror
 </div>
 <div class="mb-3">
-    <label for="amount" class="form-label">Amount (£)</label>
     <input type="text" class="form-control @error('amount') is-invalid @enderror" id="amount"
         aria-describedby="Registration Date" name="amount" value="{{ old('amount') }}@isset($service_payment_record) {{ $service_payment_record->amount }} @endisset">
     @error('amount')
